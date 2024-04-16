a4981158f25ab5a69325adb1e2cf35746ab5043b
rishavConsoleLog21 <kumarrishavcool21@gmail.com>
2024-04-16 16:44:31 +0530

    + formatted price

--- src/components/MealItem.jsx
+++ src/components/MealItem.jsx
@@ -1,3 +1,5 @@
+import {formatPrice} from '../util/formatting.js'; 
+
 const MealItem = ({ meal }) => {
   return (
     <li className="meal-item">
@@ -5,7 +7,7 @@ const MealItem = ({ meal }) => {
         <img src={`http://localhost:3000/${meal.image}`} alt={meal.name} />
         <div>
           <h3>{meal.name}</h3>
-          <p className="meal-item-price">{meal.price}</p>
+          <p className="meal-item-price">{formatPrice.format(meal.price)}</p>
           <p className="meal-item-description">{meal.description}</p>
         </div>
       </article>
